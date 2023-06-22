9c312f0fad4ec254ab0cc8adee09510aa083942a
JeonByeongHyeon <iedl97@naver.com>
2023-06-22 15:34:20 +0900

    Member,SeriveBoardDAO getInstance,closeAll 추가

--- semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/model/MemberDAO.java
+++ semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/model/MemberDAO.java
@@ -1,5 +1,30 @@
 package org.kosta.jeongdab.tou.model;
 
-public class MemberDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private static MemberDAO instance=new MemberDAO();
+	private DataSource dataSource;
+	private MemberDAO() {
+		this.dataSource=DataSourceManager.getInstance().getDataSource();
+	}
+	public static MemberDAO getInstance()   {			
+		return instance;
+	}
+	public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException {
+		if(pstmt!=null)
+			pstmt.close();
+		if(con!=null)
+			con.close(); // connection을 dbcp에 반환한다 
+	}
+	public void closeAll(ResultSet rs,PreparedStatement pstmt,Connection con) throws SQLException {
+		if(rs!=null)
+			rs.close();
+		closeAll(pstmt, con);
+	}
 }
