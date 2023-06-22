d0e664e400676bc422e47734fccdc2e1ba640c31
cyon8254 <cyon8254@gmail.com>
2023-06-22 17:36:19 +0900

    [박해준] email 인증 테스트, password 암호화 테스트

---
+++ semiProject-JeongDab-Check-In-To-You/src/test/java/org/kosta/jeongdab/test2/TestPasswordEncryption.java
@@ -0,0 +1,32 @@
+package org.kosta.jeongdab.test2;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+// 비밀번호 암호화 테스트
+
+public class TestPasswordEncryption {
+
+	// 비밀번호를 해시화하여 저장하는 메서드
+	public static String hashPassword(String password) {
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+		return hashedPassword;
+	}
+
+	// 저장된 해시화된 비밀번호를 확인하는 메서드
+	public static boolean checkPassword(String password, String hashedPassword) {
+		boolean passwordMatch = BCrypt.checkpw(password, hashedPassword);
+		return passwordMatch;
+	}
+
+	public static void main(String[] args) {
+		String originalPassword = "password123";
+
+		// 비밀번호를 해시화하여 저장
+		String hashedPassword = hashPassword(originalPassword);
+		System.out.println("Hashed Password: " + hashedPassword);
+
+		// 저장된 해시화된 비밀번호 확인
+		boolean isMatch = checkPassword(originalPassword, hashedPassword);
+		System.out.println("Password Match: " + isMatch);
+	}
+}
