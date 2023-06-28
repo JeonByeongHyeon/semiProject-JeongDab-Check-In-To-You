ae11e8265ae1bb8c74f5b58f39237204140dc52f
JeonByeongHyeon <iedl97@naver.com>
2023-06-28 10:44:48 +0900

    [전병현] 댓글 삭제 수정

--- semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/controller/DeleteReplyAjaxController.java
+++ semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/controller/DeleteReplyAjaxController.java
@@ -1,5 +1,6 @@
 package org.kosta.jeongdab.tou.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,9 +14,15 @@ public class DeleteReplyAjaxController implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (request.getMethod().equals("POST") == false)
+			throw new ServletException("POST 방식만 서비스 됩니다");
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("member") == null) {
+			System.out.println("인증안돼서 댓글수정 안해줌!");
+			return "redirect:FindServiceBoardByNo.do";
+		}
 		String message = null;
 		String replyNo = request.getParameter("no");
-		HttpSession session=request.getSession(false);
 		long no = Long.parseLong(replyNo);
 		MemberVO memberVO = new MemberVO();
 		memberVO = (MemberVO) session.getAttribute("member");
