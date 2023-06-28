ddcd9e57a1cbbc1a386fbbb9080fb4bc5933478a
JeonByeongHyeon <iedl97@naver.com>
2023-06-28 13:19:01 +0900

    [전병현] 미사용 import제거

--- semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/controller/DeleteReplyAjaxController.java
+++ semiProject-JeongDab-Check-In-To-You/src/main/java/org/kosta/jeongdab/tou/controller/DeleteReplyAjaxController.java
@@ -1,14 +1,9 @@
 package org.kosta.jeongdab.tou.controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
-import org.kosta.jeongdab.tou.model.MemberVO;
 import org.kosta.jeongdab.tou.model.ReplyDAO;
-import org.kosta.jeongdab.tou.model.ReplyVO;
 
 public class DeleteReplyAjaxController implements Controller {
 
